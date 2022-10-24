1fb31a16738d60b0a8b66c23b13dd6a33ea29f78
Marknjo <marknjo7@gmail.com>
2022-10-24 10:47:08 +0300

    feat(c-g): add initial loading state display to the ui

--- basic-client/src/main.ts
+++ basic-client/src/main.ts
@@ -1,1 +1,8 @@
 import "./style.css";
+
+const rootEl = document.getElementById("app");
+const displayEl = document.querySelector(".greeting") as HTMLElement;
+
+if (displayEl) {
+  displayEl.textContent = "Loading...";
+}
