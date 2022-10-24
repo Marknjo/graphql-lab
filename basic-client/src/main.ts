824f06fd9ba52d2a82e2e8b95957619493f06396
Marknjo <marknjo7@gmail.com>
2022-10-24 10:56:57 +0300

    feat(c-g): auto fetch greetings from the graphql server - vannila js/ts

--- basic-client/src/main.ts
+++ basic-client/src/main.ts
@@ -1,8 +1,41 @@
 import "./style.css";
 
-const rootEl = document.getElementById("app");
+// const rootEl = document.getElementById("app");
 const displayEl = document.querySelector(".greeting") as HTMLElement;
 
 if (displayEl) {
   displayEl.textContent = "Loading...";
 }
+
+async function fetchGreetings() {
+  try {
+    const response = await fetch("http://localhost:9000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+          greeting
+        }
+      `,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorMsg = `${response.status}, ${response.statusText}, "Repose failed"`;
+      throw new Error(errorMsg);
+    }
+
+    const {
+      data: { greeting },
+    } = await response.json();
+
+    displayEl.textContent = greeting;
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
+
+fetchGreetings();
